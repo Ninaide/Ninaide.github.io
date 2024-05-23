2b92ad660624d08d1102c8a674321b6a65ba8d6c
Ninaide <s3958326@student.rmit.edu.au>
2024-05-23 19:15:44 +1000

    added fast forwards and rewind buttons

---
+++ assignment2/script.js
@@ -0,0 +1,35 @@
+const video = document.querySelector("#custom-video-player");
+const playPauseBtn = document.querySelector("#play-pause-btn");
+const playPauseImg = document.querySelector("#play-pause-img");
+const progressBar = document.querySelector("#progress-bar-fill");
+video.removeAttribute("controls");
+// playPauseBtn.addEventListener("click", togglePlayPause);
+video.addEventListener("timeupdate", updateProgressBar);
+function togglePlayPause() {
+  if (video.paused || video.ended) {
+    video.play();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+  } else {
+    video.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+  }
+}
+function updateProgressBar() {
+  const value = (video.currentTime / video.duration) * 100;
+  progressBar.style.width = value + "%";
+}
+// Add other functionalities here
+
+// Fast forwarding video by 10 seconds
+function fastForward() {
+  var sound = document.getElementById("custom-video-player");
+  sound.currentTime += 10;
+}
+
+// Rewinds video by 10 seconds
+function rewind() {
+  video.currentTime -= 10;
+  if (video.currentTime < 0) {
+    video.currentTime = 0;
+  }
+}
