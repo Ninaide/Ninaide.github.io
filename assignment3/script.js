d4d46903d2f1d261411af8b69a343022aaf988f1
Ninaide <s3958326@student.rmit.edu.au>
2024-06-18 15:37:34 +1000

    added inspiration in the css section

--- assignment3/script.js
+++ assignment3/script.js
@@ -3,6 +3,10 @@
 // However, if the image doesn't get dragged onto a coloured box (dragged anywhwere else on the screen),
 // the image disappears and the page needs to be reset. I have no clue why this happens, but the code does
 // allow for the icons to be placed on the coloured boxes so I would call that a success.
+
+// I did have some trouble getting the js to function properly and as such I did search up my issues to see if I could find a solution to
+// the issues I was having. I used a combination of YouTube, W3schools and ChatGPT. While I did use these resouces to make the code functional,
+// it was not written up externally. Below is what ended up working to ensure the drag and drop function was a success.
 document.addEventListener("DOMContentLoaded", (event) => {
   const images = document.querySelectorAll(".draggable");
   const boxes = document.querySelectorAll(".box");
